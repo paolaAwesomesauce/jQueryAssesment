ad1bb65bc6daecf050e3f9fceeb4356e8bb1c95f
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-04-19 12:04:48 -0700

    trying to make this work

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test</title>
+</head>
+<body>
+	<div class="Dan">
+		<p>
+			give me an A!
+		</p>
+	</div>
+	<div id="iwao">
+		<p>
+			u should give me an A!
+		</p>
+	</div>
+<script type="text/main.js"></script>
+</body>
---
+++ main.js
@@ -0,0 +1,14 @@
+// $( "Dan" ).click(function() {
+    // console.log( "Hey you clicked on me" );
+// });
+
+// $( "iwao" ).click(function() {
+    // console.log( "Hey you clicked on me again" );});
+
+
+$( document ).ready(function(){
+    $( "alert" ).on( "click", function() {
+        alert( "Hey ypu clicked on me" );
+    });
+ 
+});
